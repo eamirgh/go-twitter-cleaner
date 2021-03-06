3f13269de086d106e5107f270fd4b29781bed7e3
Amir Ghaffari <a.g.h.94.72@gmail.com>
2021-03-06 09:18:05 +0330

    removed keep alive

--- main.go
+++ main.go
@@ -46,6 +46,5 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hydrate)
 	srv := &http.Server{Addr: "0.0.0.0:" + os.Getenv("PORT"), ReadTimeout: 15 * time.Second, WriteTimeout: 15 * time.Second, Handler: mux}
-	go keepAlive()
 	go log.Fatal(srv.ListenAndServe())
 }
